c2d5e270de6477ea99926da74c4c6d2610a58732
tyleryannes94 <tyler.yannes@gmail.com>
2023-12-09 14:53:50 -0600

    Added a promptUser function to initiate the input questions and answer for the user.

--- index.js
+++ index.js
@@ -34,3 +34,30 @@ function createSVG({ text, textColor, shape, shapeColor }) {
         }
     });
 }
+
+function promptUser() {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the logo:',
+        validate: input => input.length <= 3 ? true : 'Maximum 3 characters allowed.'
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the text color (keyword or hex):',
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['circle', 'triangle', 'square']
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the shape color (keyword or hex):',
+      },
+    ]);
+  }
